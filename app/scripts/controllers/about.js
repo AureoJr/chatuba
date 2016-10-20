d34bd5f49edc0635e0aaad1800b45eeb8ca3b15f
Aureo Júnior <aureojr1@gmail.com>
2016-10-20 01:40:32 -0200

    primeiro commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chatubaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the chatubaApp
+ */
+angular.module('chatubaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chatubaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the chatubaApp
+ */
+angular.module('chatubaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
